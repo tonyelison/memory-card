35c7b7e440b738c1f4756e0e884b8bc38d741e9e
Anthony Elison <tonyelison37@gmail.com>
2023-10-27 15:06:19 -0600

    Add failsafe image for sprite-less results, add loading logic

--- src/components/card-list.jsx
+++ src/components/card-list.jsx
@@ -3,9 +3,10 @@ import api from '../api.js'
 import Pokemon from './pokemon.jsx'
 
 const CardList = () => {
-  const [shuffledList, setShuffledList] = useState([]);
+  const [cardList, setCardList] = useState([]);
   const [blackList, setBlackList] = useState([]);
-
+  const [isLoading, setIsLoading] = useState(false);
+  
   const randomInt = (max) => Math.floor(Math.random() * max);
   
   const generateRandomIndices = (max, length) => {
@@ -19,22 +20,29 @@ const CardList = () => {
     return indices;
   };
 
-  const shuffleList = (list) => {
-    for (let i = list.length - 1; i > 0; i--) {
+  const shuffle = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
         const j = randomInt(i + 1);
-        [list[i], list[j]] = [list[j], list[i]];
+        [array[i], array[j]] = [array[j], array[i]];
     }
   };
 
   const refreshList = async () => {
+    setIsLoading(true)
     return await api.getList()
-      .then((list) => {
+      .then(async (list) => {
         const randomIndices = generateRandomIndices(list.count, 10 - blackList.length);
         const newList = randomIndices.map((index) => list.results[index].name);
         
         newList.push(...blackList);
-        shuffleList(newList);
-        setShuffledList(newList);
+        shuffle(newList);
+
+        const queryForEachId = () => newList.map(async (id) => await api.getById(id));
+
+        Promise.all(queryForEachId()).then((queriedList) => {
+          setCardList(queriedList);
+          setIsLoading(false)
+        });
       })
       .catch((error) => console.log(error));
   }
@@ -54,10 +62,21 @@ const CardList = () => {
     });
   }
 
+  const renderCardList = () => {
+    return cardList.map((item) => {
+      const details = {
+        id: item.id,
+        name: item.name,
+        'image-url': item.sprites['front_default'] || './public/decamark.png',
+      };
+      return (<Pokemon key={item.id} details={details} blackList={blackList} addToBlackList={addToBlackList} resetGame={resetGame} />)
+    })
+  };
+
   return (
     <>
-      {shuffledList.map((id) => <Pokemon key={id} id={id} blackList={blackList} addToBlackList={addToBlackList} resetGame={resetGame} />)}
-      <button onClick={refreshList}>Refresh List</button>
+      {isLoading ? <div>Loading...</div> : renderCardList()}
+      <button onClick={refreshList} disabled={isLoading}>Refresh List</button>
     </>
   );
 }
--- src/components/pokemon.jsx
+++ src/components/pokemon.jsx
@@ -1,25 +1,7 @@
 import '../styles/pokemon.css'
-import { useState, useEffect } from 'react'
-import api from '../api.js'
 import humanize from '../util/humanize.js'
 
-const Pokemon = ({ id, blackList, addToBlackList, resetGame }) => {
-  const [details, setDetails] = useState('');
-
-  useEffect(() => {
-    api.getById(id)
-      .then((response) => {
-        setDetails({
-          id: response.id,
-          name: response.name,
-          'image-url': response.sprites['front_default'],
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [id]);
-
+const Pokemon = ({ details, blackList, addToBlackList, resetGame }) => {
   const didClickCard = () => {
     blackList.includes(details.name) ? resetGame() : addToBlackList(details.name);
   };
